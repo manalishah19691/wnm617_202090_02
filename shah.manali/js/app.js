9cf5b035308efe9328c956433ba889c1a7583eeb
Manali Shah <manalishah@Manalis-MacBook-Pro.local>
2020-10-06 16:32:16 -0700

    module 5

--- shah.manali/js/app.js
+++ shah.manali/js/app.js
@@ -1,1 +1,43 @@
-app.js
+
+// Document Ready
+$(()=>{
+
+   checkUserId();
+
+   $(document)
+
+   // event delegation
+   .on("submit","#signin-form",function(e){
+      e.preventDefault();
+      checkSigninForm();
+   })
+
+
+
+
+   /* anchor clicks */
+   .on("click",".js-logout",function(e){
+      sessionStorage.removeItem('userId');
+      checkUserId();
+   })
+
+
+
+
+
+
+   .on("click","[data-activate]",function(){
+      let target = $(this).data('activate');
+      $(target).addClass("active");
+   })
+   .on("click","[data-deactivate]",function(){
+      let target = $(this).data('deactivate');
+      $(target).removeClass("active");
+   })
+   .on("click","[data-toggle]",function(){
+      let target = $(this).data('toggle');
+      $(target).toggleClass("active");
+   })
+
+
+})
